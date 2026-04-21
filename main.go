77ec999df50562f38483e6951d2a7bfccf5b1f2b
Umar <umarhonsultanov3@gmail.com>
2026-04-21 17:56:56 +0500

    basic cache and compress

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"time"
+
 	"github.com/Umarxon80/Fiber.git/db"
 	"github.com/Umarxon80/Fiber.git/logger"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/log"
+	"github.com/gofiber/fiber/v3/middleware/cache"
+	"github.com/gofiber/fiber/v3/middleware/compress"
 )
 
 func main() {
-	log.SetOutput(logger.Logger()) // logger set up
+	log.SetOutput(logger.Logger())             // logger set up
+	cacheMiddleware := cache.New(cache.Config{ // caching
+		Expiration: 10 * time.Second,
+	})
 
 	app := fiber.New(fiber.Config{
 		AppName: "Fiber",
 	})
 
 	// Get
-	app.Get("/", func(c fiber.Ctx) error {
+	app.Get("/", compress.New(), cacheMiddleware, func(c fiber.Ctx) error {
 		log.Info("Products are shown")
 		return c.JSON(db.Get())
 	})
@@ -24,6 +31,7 @@ func main() {
 	app.Post("/", func(c fiber.Ctx) error {
 		var p db.Product
 		if err := c.Bind().JSON(&p); err != nil {
+			log.Errorf("Wrong input: %v", err)
 			return err
 		}
 
@@ -36,7 +44,7 @@ func main() {
 		id := fiber.Params[int](c, "id")
 		var p db.Product
 		if err := c.Bind().JSON(&p); err != nil {
-			log.Errorf("Wrong input: %s", c.Body())
+			log.Errorf("Wrong input: %v", err)
 			return err
 		}
 
